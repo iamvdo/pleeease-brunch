39a91e24d6994527f6c5a160a47a70cdacc39bbd
Vincent De Oliveira <vincent@iamvdo.me>
2015-08-13 14:23:18 +0200

    Add tests

---
+++ test/test.js
@@ -0,0 +1,60 @@
+var Plugin = require("../lib");
+var fs = require('fs');
+var should = require('should');
+
+describe('Plugin', function () {
+
+  var plugin, config;
+
+  beforeEach(function() {
+    config = {
+      paths: {root: '.'},
+      optimize: true,
+      plugins: {
+        pleeease: {
+          autoprefixer: {browsers: 'last 99 versions'},
+          mqpacker: true
+        }
+      }
+    };
+    plugin = new Plugin(config);
+  });
+
+  it('should be an object', function() {
+    plugin = new Plugin(config);
+    plugin.should.be.ok;
+  });
+
+  it('optimize', function (done) {
+    var css = 'a{a:a}';
+    plugin.optimize({data: css}, function (err, data) {
+      data.data.should.be.eql(css);
+      done();
+    });
+  });
+
+  it('optimize with options', function (done) {
+    var css = fs.readFileSync('test/fixtures/sample.css', 'utf-8');
+    var expected = fs.readFileSync('test/fixtures/sample.out.css', 'utf-8');
+    plugin.optimize({data: css}, function (err, data) {
+      data.data.should.be.eql(expected);
+      done();
+    });
+  });
+
+  it('optimize with sourcemaps', function (done) {
+    var css = fs.readFileSync('test/fixtures/sample.css', 'utf-8');
+    var map = {
+      version: 3,
+      sources: [ 'sample.css' ],
+      names: [],
+      mappings: 'AAKA,QACE,oBAAc,AAAd,qBAAc,AAAd,iBAAc,AAAd,oBAAc,AAAd,YAAc,CACf,AAPD,cACE,GACE,UAAY,CACb,AAMD,GACE,UAAY,CACb,CAPF',
+      file: 'sample.css'
+    };
+    plugin.optimize({data: css, path: 'test/fixtures/sample.css'}, function (err, data) {
+      data.map.should.be.eql(map);
+      done();
+    });
+  });
+
+});
